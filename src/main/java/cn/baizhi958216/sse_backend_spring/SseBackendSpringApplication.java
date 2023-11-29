654ab97af484482b2581c4f07fceba62c83f4389
baizhi958216 <1475289190@qq.com>
2023-11-30 07:49:13 +0800

    feat: Springboot Initialization
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ src/main/java/cn/baizhi958216/sse_backend_spring/SseBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package cn.baizhi958216.sse_backend_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseBackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseBackendSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/baizhi958216/sse_backend_spring/SseBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.baizhi958216.sse_backend_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseBackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
